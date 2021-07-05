d4e1723d75ee0a8d044ae9359c33b84bdf2504ea
Hunter Lovell <hunter@cubbys.co>
2021-07-05 09:08:45 -0600

    fix: sql connection closing

--- src/connection.js
+++ src/connection.js
@@ -6,9 +6,21 @@ module.exports = function(options) {
     let opts = options || {};
     this.sql = mysql.createConnection(opts);
 
-    this.query = async function query(queryString) {
+    this.query = async function query(queryString, _retry = 1) {
         let sql = await this.sql;
-        return (await sql.execute(queryString))[0];
+        return sql.ping()
+          .then(async () => {
+            let [results, fields] = await sql.execute(queryString);
+            return results;
+          })
+          .catch(() => {
+            if (_retry > 3) {
+              this.sql = mysql.createConnection(opts);
+              this.query(queryString, _retry + 1)
+            } else {
+              throw new Error('MYSQL failed to reconnect');
+            }
+          })
     }
 
     return (req, _, next) => {
